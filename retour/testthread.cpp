835543777c12a75e3e460b4adec902fe0e8bb8ee
= <=>
2019-05-27 00:31:14 +0200

    modif à corriger

--- retour/testthread.cpp
+++ retour/testthread.cpp
@@ -1,13 +1,42 @@
 #include <SFML/System/Clock.hpp>
 #include <SFML/System/Time.hpp>
+#include <SFML/System/Thread.hpp>
 #include <iostream>
+#include <thread>
+#include <SFML/System/Mutex.hpp>
+
+void affiche(int *nombrethread, sf::Mutex *m)
+{
+    m->lock();
+    for (int i = 0; i <= 10; i++)
+    {
+        std::cout <<"thread"<< nombrethread <<std::endl;;
+    }
+    m->unlock();
+}
 
 int main()
-{
+{   
+    sf::Mutex m1;
+    int nbr(1);
+    int nbr2(2);
+    int *nbr3 = new int(5);
+    sf::Thread t1(&affiche,&nbr, &m1 );
+    sf::Thread t2 (&affiche,&nbr2,&m1);
+    //std::thread t3(affiche,3);//
+    
+    t1.launch();
+    t2.launch();
+    for (int i = 0; i <= 10; i++)
+    {
+        std::cout <<"thread principal"<<std::endl;;
+    }
+
     sf::Time premiere = sf::microseconds(1000);
     int test = premiere.asMicroseconds();
     std::cout << test << std::endl;
     
 
-
+//t3.join();//
+return 0;
 }
